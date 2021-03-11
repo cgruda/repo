e7d1f6fdbe24b9c9e33bccbf212865d247bdfcc7
chaim <cgruda@gmail.com>
2021-03-12 00:48:36 +0200

    add visual_studio project

--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 <!-- ABOUT THE PROJECT -->
 ## About The Project
 
-ZedBoard FPGA based Convolutional Neural Network (CNN) accelerator
+ZedBoard FPGA based Convolutional Neural Network (CNN) accelerator.
+
+
+
+
 
 <!-- GETTING STARTED -->
-### Prerequisites
 
+The project was implemented using the following programs:
 
 * []() Vivado HLS 2016.4
 * []() Vivado HLx 2016.4
 * []() Vivado SDK 2016.4
-* []() ZedBoard
+* []() Visual Studio 2019
+
 
 This is an example of how to list things you need to use the software and how to install them.
 * exmaple
---
+++ VisualStudio/CNN/CNN/cnn.h
@@ -0,0 +1,63 @@
+/**
+ * Project 20-1-1-2187
+ * CNN accelerator
+ *
+ * Chaim Gruda
+ * Shay Tsabar
+ *
+ */
+
+#ifndef __CNN_H__
+#define __CNN_H__
+
+/*
+ * INCLUDES
+ ******************************************************************************
+ */
+
+#include "matrix.h"
+
+/*
+ * ENUMERATIONS
+ ******************************************************************************
+ */
+
+enum pooling_type {
+	POOL_AVG,
+	POOL_MAX
+};
+
+/*
+ * STRUCTIRES
+ ******************************************************************************
+ */
+
+struct env {
+	matrix_t m_image;
+	matrix_t m_kernel;
+	matrix_t m_bias;
+	matrix_t m_conv_result;
+	matrix_t m_pool_result;
+};
+
+/*
+ * DECLARATION
+ ******************************************************************************
+ */
+
+int pool_2D(matrix_t *input, int pool_dim, matrix_t *output, int max_pool);
+
+int conv_2D(matrix_t *input, matrix_t *kernel, matrix_t *bias, matrix_t *output);
+
+int conv_2D_norm(matrixf_t *input, matrix_t *kernel, matrix_t *bias, matrixf_t *output);
+
+int cnn_sw(struct env *cnn_env);
+
+int cnn_sw_norm(struct env *cnn_env);
+
+int cnn_hw(struct env *cnn_env);
+
+int cnn_precision_loss_calc(matrix_t *ref, matrix_t *test, double *prec);
+
+
+#endif // __CNN_H__
